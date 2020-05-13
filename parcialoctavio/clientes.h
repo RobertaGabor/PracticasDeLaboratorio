4410d98f3f8704363a84edc16ba386c7249e3d35
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-05-13 02:44:51 -0300

    Add files via upload

---
+++ parcialoctavio/clientes.h
@@ -0,0 +1,17 @@
+#ifndef CLIENTES_H_INCLUDED
+#define CLIENTES_H_INCLUDED
+
+typedef struct{
+
+char nombre[30];
+char apellido[30];
+char localidad[30];
+long double telefono;
+int edad;
+char sexo;
+int id;
+int isEmpty;
+
+}eClientes;
+
+#endif // CLIENTES_H_INCLUDED
