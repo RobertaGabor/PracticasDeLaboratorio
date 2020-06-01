68af295426a2d818cefa59de3f0a8551d9f26774
Your Name <you@example.com>
2020-06-01 12:31:50 -0300

    yanomaswey

--- practicas/OnlineClasses/memoriaDinamicaClase15/main.c
+++ practicas/OnlineClasses/memoriaDinamicaClase15/main.c
@@ -8,7 +8,6 @@ int main()
     x=10;
     printf("x vale %d\n",x);
 
-
     /*dinamico*/
     int* p=(int*)malloc(sizeof(int));
     /*casteo de puntero void(malloc devuelve eso) a puntero entero que es lo que pido*/
@@ -23,11 +22,68 @@ int main()
 
     *p=10;
     printf("p vale %d\n",*p);
+
+
+    /*VECTORES*/
+    printf("\n---------------------\n");
+    int y[5]={1,2,3,4,5};
+    for(int i=0;i<5;i++)
+    {
+        printf("%d\n",y[i]);
+    }
+
+
+    int* q=(int*)malloc(5 * sizeof(int));
+    int* aux;
+    /*pido datos*/
+    for(int i=0;i<5;i++)
+    {
+        printf("Ingrese un numero: ");
+        scanf("%d",q+i);/*con dinamico es + i*/
+    }
+    printf("\n\n");
+    /*muestro datos*/
+    for(int i=0;i<5;i++)
+    {
+        printf("%d",*(q+i));
+    }
+/*agrandar*/
+    aux=(int*)realloc(q,10 * sizeof(int));/*pones el total, no cuanto queres agrandar*/
+    if(aux!=NULL)
+    {
+        q=aux;
+        printf("\nSe pudo conseguir lugar\n");
+
+        for(int i=5/*porque ya cargue 5*/;i<10;i++)
+        {
+            printf("Ingrese un numero: ");
+            scanf("%d",q+i);
+        }
+        printf("\n\n");
+        for(int i=0; i<10; i++)
+        {
+            printf("%d",*(q+i));
+        }
+/*achicar*/
+        printf("\n\n");
+        q=(int*)realloc(q,6 * sizeof(int));
+        for(int i=0; i<6; i++)
+        {
+            printf("%d",*(q+i));
+        }
+/*inicializar en 0*/
+        printf("\n\n");
+        int* vec=(int*)calloc(6,sizeof(int));
+        for(int i=0; i<6; i++)
+        {
+            printf("%d",*(vec+i));
+        }
+    }
+    else
+    {
+        printf("No se pudo aumentar los bytes\n");
+    }
+
     free(p);
-
-
-
-
-
     return 0;
 }
